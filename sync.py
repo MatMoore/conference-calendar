f5a50190306e92bb29881f9f1166ef3671f570df
Mat Moore <matmoore@users.noreply.github.com>
2020-02-24 20:38:45 +0000

    Introduce CsvParser and Planner classes
    I want these to be objects that I can pass as dependencies to other objects.

--- sync.py
+++ sync.py
@@ -45,35 +45,43 @@ class MalformedCSV(Exception):
     """
 
 
-def parse_events(csv_file) -> Set[Event]:
-    """
-    Parse a CSV file and return a list of events
-    """
-    results = set()
-    csv_reader = DictReader(csv_file)
+def parse_events(csv_file):
+    return CsvParser(csv_file).parse_events()
 
-    if csv_reader.fieldnames != HEADER_NAMES:
-        raise MalformedCSV(f'Expected header to equal {HEADER_NAMES} but got {csv_reader.fieldnames}')
 
-    for row in csv_reader:
-        try:
-            start_date = date.fromisoformat(row['start_date'])
-            end_date = date.fromisoformat(row['end_date'])
-            website = row['website']
-            description = row['description']
-        except (KeyError, ValueError) as exc:
-            raise MalformedCSV from exc
+class CsvParser:
+    def __init__(self, csv_file):
+        self.csv_file = csv_file
 
-        event = Event(
-            start_date=start_date,
-            end_date=end_date,
-            website=website,
-            description=description
-        )
+    def parse_events(self) -> Set[Event]:
+        """
+        Parse a CSV file and return a list of events
+        """
+        results = set()
+        csv_reader = DictReader(self.csv_file)
 
-        results.add(event)
+        if csv_reader.fieldnames != HEADER_NAMES:
+            raise MalformedCSV(f'Expected header to equal {HEADER_NAMES} but got {csv_reader.fieldnames}')
 
-    return results
+        for row in csv_reader:
+            try:
+                start_date = date.fromisoformat(row['start_date'])
+                end_date = date.fromisoformat(row['end_date'])
+                website = row['website']
+                description = row['description']
+            except (KeyError, ValueError) as exc:
+                raise MalformedCSV from exc
+
+            event = Event(
+                start_date=start_date,
+                end_date=end_date,
+                website=website,
+                description=description
+            )
+
+            results.add(event)
+
+        return results
 
 
 def plan_changes(calendar_id: int, existing_events: Dict[Event, int], desired_events: Set[Event]) -> Plan:
@@ -81,17 +89,25 @@ def plan_changes(calendar_id: int, existing_events: Dict[Event, int], desired_ev
     Compare the old and new events and work out what changes to make
     against the API
     """
-    existing_event_set = set(existing_events.keys())
-    events_to_remove = existing_event_set - desired_events
-    events_to_add = desired_events - existing_event_set
+    return Planner(calendar_id).plan(existing_events, desired_events)
 
-    ids_to_remove = {existing_events[event_id] for event_id in events_to_remove}
 
-    return Plan(
-        calendar_id=calendar_id,
-        to_remove=ids_to_remove,
-        to_add=events_to_add
-    )
+class Planner:
+    def __init__(self, calendar_id):
+        self.calendar_id = calendar_id
+
+    def plan(self, existing_events: Dict[Event, int], desired_events: Set[Event]) -> Plan:
+        existing_event_set = set(existing_events.keys())
+        events_to_remove = existing_event_set - desired_events
+        events_to_add = desired_events - existing_event_set
+
+        ids_to_remove = {existing_events[event_id] for event_id in events_to_remove}
+
+        return Plan(
+            calendar_id=self.calendar_id,
+            to_remove=ids_to_remove,
+            to_add=events_to_add
+        )
 
 
 class CalendarAPI:
