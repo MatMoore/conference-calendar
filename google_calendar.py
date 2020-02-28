9ca17819801fe56e2eab1836b1675e4002044bed
Mat Moore <matmoore@users.noreply.github.com>
2020-02-28 10:35:23 +0000

    fail

--- google_calendar.py
+++ google_calendar.py
@@ -12,6 +12,7 @@ def parse_event_body(event_response) -> Event:
     """
     Parse an event from google calendar
     """
+    raise Exception('test')
     start_date = date.fromisoformat(event_response['start']['date'])
     end_date = date.fromisoformat(event_response['end']['date'])
 
