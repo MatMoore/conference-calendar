6e5c7919f2f02d849c79a9c0c9227acfdd19288d
Mat Moore <matmoore@users.noreply.github.com>
2020-02-28 10:09:47 +0000

    Extract CalendarAPI class into google_calendar module

--- google_calendar.py
+++ google_calendar.py
@@ -1,4 +1,10 @@
-from datetime import date
+from typing import Dict
+from datetime import date, datetime
+from httplib2 import Http
+import ast
+import os
+from googleapiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
 from models import Event
 
 
@@ -45,3 +51,64 @@ def build_event_body(event: Event):
             },
         },
     }
+
+
+
+class CalendarAPI:
+    SCOPES = ['https://www.googleapis.com/auth/calendar.events']
+    CALENDAR_ID = os.environ['CALENDAR_ID']
+
+    def __init__(self):
+        client_secret_dict = ast.literal_eval(os.environ['CLIENT_SECRET_JSON'])
+        credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+            client_secret_dict, scopes=self.SCOPES)
+        http = credentials.authorize(Http())
+        self.calendar = build('calendar', 'v3', http=http)
+
+    def remove(self, event_id):
+        """
+        Remove an event from the calendar
+        """
+        print(f'removing {event_id}')
+
+        request = self.calendar.events().delete(calendarId=self.CALENDAR_ID, eventId=event_id)
+        request.execute()
+
+    def add(self, event):
+        """
+        Add an event to the calendar
+        """
+        print(f'adding {event}')
+
+        body = build_event_body(event)
+
+        request = self.calendar.events().insert(calendarId=self.CALENDAR_ID, body=body)
+        request.execute()
+
+    def fetch_events(self) -> Dict[Event, int]:
+        """
+        Fetch events from the remote calendar.
+        Returns a dictionary where the key is the event details and the value is the event ID
+
+        See https://googleapis.github.io/google-api-python-client/docs/dyn/calendar_v3.events.html#list
+        """
+        now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+
+        request = self.calendar.events().list(
+            calendarId=self.CALENDAR_ID,
+            timeMin=now,
+            maxResults=10,
+            singleEvents=True,
+            orderBy='startTime'
+        )
+        events_result = request.execute()
+        raw_events = events_result.get('items', [])
+
+        events = {}
+
+        for raw_event in raw_events:
+            event_id = raw_event['id']
+            event = parse_event_body(raw_event)
+            events[event] = event_id
+
+        return events
--- sync.py
+++ sync.py
@@ -4,17 +4,11 @@ Load events from Calendar
 Calculate Removals, Additions
 Execute
 """
-from typing import NamedTuple, Set, Dict, Optional
+from typing import Set, Dict
 from datetime import date
 from csv import DictReader
-from httplib2 import Http
-import ast
-import os
-import datetime
-from googleapiclient.discovery import build
-from oauth2client.service_account import ServiceAccountCredentials
 from models import Event, Plan
-import google_calendar
+from google_calendar import CalendarAPI
 
 HEADER_NAMES = ['start_date', 'end_date', 'title', 'website', 'description']
 
@@ -90,66 +84,6 @@ class Planner:
             to_remove=ids_to_remove,
             to_add=events_to_add
         )
-
-
-class CalendarAPI:
-    SCOPES = ['https://www.googleapis.com/auth/calendar.events']
-    CALENDAR_ID = os.environ['CALENDAR_ID']
-
-    def __init__(self):
-        client_secret_dict = ast.literal_eval(os.environ['CLIENT_SECRET_JSON'])
-        credentials = ServiceAccountCredentials.from_json_keyfile_dict(
-            client_secret_dict, scopes=self.SCOPES)
-        http = credentials.authorize(Http())
-        self.calendar = build('calendar', 'v3', http=http)
-
-    def remove(self, event_id):
-        """
-        Remove an event from the calendar
-        """
-        print(f'removing {event_id}')
-
-        request = self.calendar.events().delete(calendarId=self.CALENDAR_ID, eventId=event_id)
-        request.execute()
-
-    def add(self, event):
-        """
-        Add an event to the calendar
-        """
-        print(f'adding {event}')
-
-        body = google_calendar.build_event_body(event)
-
-        request = self.calendar.events().insert(calendarId=self.CALENDAR_ID, body=body)
-        request.execute()
-
-    def fetch_events(self) -> Dict[Event, int]:
-        """
-        Fetch events from the remote calendar.
-        Returns a dictionary where the key is the event details and the value is the event ID
-
-        See https://googleapis.github.io/google-api-python-client/docs/dyn/calendar_v3.events.html#list
-        """
-        now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-
-        request = self.calendar.events().list(
-            calendarId=self.CALENDAR_ID,
-            timeMin=now,
-            maxResults=10,
-            singleEvents=True,
-            orderBy='startTime'
-        )
-        events_result = request.execute()
-        raw_events = events_result.get('items', [])
-
-        events = {}
-
-        for raw_event in raw_events:
-            event_id = raw_event['id']
-            event = google_calendar.parse_event_body(raw_event)
-            events[event] = event_id
-
-        return events
 
 
 class Syncer:
